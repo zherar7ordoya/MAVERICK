45461081a01089ebc316222e7261b4d87f189f7e
Gerardo Tordoya <gtordoya@TRIBJUY.local>
2021-03-31 09:20:08 -0300

    Top Gun

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const chalk = require("chalk");
-const log = console.log;
+const log = console.info;
+
+console.table([
+  {
+    Fecha: new Date("2021-03-27T00:00-0300").toDateString(),
+    Bitácora: "Veo la presentación de KeystoneJS",
+  },
+  {
+    Fecha: new Date("2021-03-29T00:00-0300").toDateString(),
+    Bitácora: "Uso «colors», pero no tiene anaranjado",
+  },
+  {
+    Fecha: new Date("2021-03-31T00:00-0300").toDateString(),
+    Bitácora: "Uso «chalk» con excelentes resultados",
+  }
+]);
 
 log("\n");
 log(
